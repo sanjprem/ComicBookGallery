96d58312fd712965a9298fee3e7bdd72b820493b
Sanj Prem <sanj.tools@gmail.com>
2018-03-15 12:02:59 -0400

    added Series model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,7 +8,7 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public Series Series { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
@@ -18,7 +18,15 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle + " #" + IssueNumber;
+                var series = Series;
+                if (series != null)
+                {
+                    return Series.Title + " #" + IssueNumber;
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
         // series-title-issuenumber.jpg
@@ -26,8 +34,16 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-")
-                    .ToLower() + "-" + IssueNumber + ".jpg";
+                var series = Series;
+                if (series != null)
+                {
+                    return Series.Title.Replace(" ", "-")
+                        .ToLower() + "-" + IssueNumber + ".jpg";
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
     }
---
+++ src/ComicBookGallery/Models/Series.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Series
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string DescriptionHtml { get; set; }
+    }
+}
