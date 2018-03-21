fa1be525b6fbe40b2fe60f89e8bc0e87e930f29f
Sanj Prem <sanj.tools@gmail.com>
2018-03-21 16:04:32 -0400

    Added series repository

---
+++ src/ComicBookGallery/Data/SeriesRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Data
+{
+    public class SeriesRepository
+    {
+        public Series[] GetSeries()
+        {
+            return Data.Series;
+        }
+
+        public Series GetSeriesDetail(int id)
+        {
+            SeriesRepository seriesToReturn = null;
+            
+            foreach (var series in Data.Series)
+            {
+                if (series.Id == id)
+                {
+                    seriesToReturn = series;
+                    break;
+                }
+            }
+
+            if (seriesToReturn != null)
+            {
+                var comicBooks = new ComicBook[0];
+
+                foreach (var comicBook in Data.ComicBooks)
+                {
+                    if (comicBook.Series != null && comicBook.Series.Id == id)
+                    {
+                        Array.Resize(ref comicBooks, comicBooks.Length + 1);
+                        comicBooks[comicBooks.Length - 1] = comicBook;
+                    }
+                }
+
+                seriesToReturn.Issues = comicBooks;
+            }
+
+            return seriesToReturn;
+        }
+    }
+}
